ba66dc7ae02635f1cb818bbeaa1d9af47a357605
Sandeep <sandbro7163337@gmail.com>
2024-08-13 14:24:39 +0530

    screen for webapp

--- src/pages/Screen.tsx
+++ src/pages/Screen.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
  
+import { Outlet,useNavigate } from "react-router-dom";
 
+//importing all the components Sidebar, Contact
 
 const Screen: React.FC = () => {
-  return(
-      <div className="w-full max-w-[1200px] h-[50rem] m-auto bg-gradient-to-r from-cyan-500 to-blue-500"  >
 
-              <Sidebar/>
+ 
+  const navigate=useNavigate()
+  
+  useEffect(()=>{  navigate('/contacts')},[])
+  
 
+ 
+  return (
+    <div className="w-full h-full  flex m-auto bg-gradient-to-r from-cyan-500 to-blue-500">
+      <Sidebar />
+
+      <div className="w-full h-full">
+          <Outlet/>
       </div>
-  )
+      
+    </div>
+  );
 };
 
 export default Screen;
