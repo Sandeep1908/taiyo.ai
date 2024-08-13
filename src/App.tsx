9bf6387047c6ca71ab07b7eb82d66f79499e3ae5
Sandeep <sandbro7163337@gmail.com>
2024-08-13 14:22:49 +0530

    configure react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import Screen from "./pages/Screen";
+import 'leaflet/dist/leaflet.css';
+
+import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider, RouterProps } from 'react-router-dom';
+import Contact from "./components/Contact";
+import Dashboard from "./components/Dashboard";
 const App:React.FC=()=>{
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      
+      <Route path='/' element={<Screen/>}>
+           <Route path='/contacts'  element={<Contact/>}/>
+           <Route path='/chatsandmap'  element={<Dashboard/>}/>
+
+       </Route>
+
+
+
+    )
+  );
+
   return(
-    <div className="w-full h-screen grid place-items-center bg-slate-400"> 
-      <Screen/>
+    <div className="w-full h-screen grid place-items-center overflow-hidden"> 
+      <RouterProvider router={router} />
     </div>
   )
 }
